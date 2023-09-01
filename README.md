ee75fc2928b0f7ccdcd7287f39eea485366720db
abdallah natsheh <abdnatsheh33@gmail.com>
2023-09-01 23:17:14 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# beepy-flashlight
-toggle white colour red on button press for SQFMI BEEPY 
+# SQFMI BEEPY FLASHLIGHT
+
+This Python script allows you to control RGB LED using a beepy  button. It provides the following functionality:
+
+- Toggle the LED on and off with a short button press.
+
+## Prerequisites
+
+Before running the script, make sure you have the following:
+
+- Python 3.x installed
+- RPi.GPIO library installed
+
+**Note:** You need root (superuser) privileges to access the GPIO pins. You can run the script with `sudo` as shown in the usage instructions.
+
+## Usage
+
+1. Clone this repository to your beepy or download the script (`flashlight.py`) directly.
+
+2. Run the script in the terminal with root privileges using `sudo`:
+   `sudo python3 flashlight.py`
+
+	**Note:**  you can add it as alias to run the script with one command in .bashrc  with `alias 	flash="python3 /path/to/flashlight.py"`
---
+++ flashlight.py
@@ -0,0 +1,76 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+# GPIO pin for the button
+BUTTON_PIN = 17
+
+# Constants for timing
+LONG_PRESS_DURATION = 2.0  # Time duration to detect a long press (2 seconds)
+
+# LED state
+led_on = False
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Function to handle a button press
+def handle_press(channel):
+    global button_held_start_time
+
+    if GPIO.input(channel):  # Button released
+        button_duration = time.time() - button_held_start_time
+
+        if button_duration < LONG_PRESS_DURATION:  # Short press
+            print("Short press detected")
+            toggle_white_light()
+    else:  # Button pressed
+        button_held_start_time = time.time()
+
+# Function to toggle the white light
+def toggle_white_light():
+    global led_on
+    if led_on:
+        print("Turning off the white light")
+        turn_off_white_light()
+    else:
+        print("Turning on the white light")
+        turn_on_white_light()
+
+# Function to turn on the white light
+def turn_on_white_light():
+    global led_on
+    with open('/sys/firmware/beepy/led_red', 'w') as red:
+        red.write("255")  # Turn on red component
+    with open('/sys/firmware/beepy/led_green', 'w') as green:
+        green.write("255")  # Turn on green component
+    with open('/sys/firmware/beepy/led_blue', 'w') as blue:
+        blue.write("255")  # Turn on blue component
+    led_on = True
+
+# Function to turn off the white light
+def turn_off_white_light():
+    global led_on
+    with open('/sys/firmware/beepy/led_red', 'w') as red:
+        red.write("0")  # Turn off red component
+    with open('/sys/firmware/beepy/led_green', 'w') as green:
+        green.write("0")  # Turn off green component
+    with open('/sys/firmware/beepy/led_blue', 'w') as blue:
+        blue.write("0")  # Turn off blue component
+    led_on = False
+
+# Initialize variable
+button_held_start_time = 0
+
+# Add event detection for the button press
+GPIO.add_event_detect(BUTTON_PIN, GPIO.BOTH, callback=handle_press, bouncetime=50)
+
+try:
+    while True:
+        time.sleep(1)  # Keep the script running
+
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    GPIO.cleanup()
